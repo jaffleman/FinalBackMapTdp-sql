09acbbb8eecf0417e47cee93a0f7f01207761b1a
jaffleman <jaffleman@gmail.com>
2022-08-07 20:35:52 +0200

    separation des routes

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const tdpController = require("./controller/tdpController");
+const geoController = require("./controller/geoController");
 
 const router = express.Router();
 
@@ -11,5 +12,11 @@ router.post("/tdp/create", tdpController.create);
 router.put("/tdp/update", tdpController.update);
 router.delete("/tdp/delete", tdpController.delete);
 router.get("/tdp/updateid", tdpController.updateid);
+
+router.post("/geolock/search", geoController.search);
+router.post("/geolock/create", geoController.create);
+router.put("/geolock/update", geoController.update);
+router.delete("/geolock/delete", geoController.delete);
+router.get("/geolock/updateid", geoController.updateid);
 
 module.exports = router;
